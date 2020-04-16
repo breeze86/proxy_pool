3eed9ab4217bae53bea7114fa4a7695fdcf85b79
breeze.yang.tech <breeze.yang.tech@linkkt.one>
2020-04-16 18:02:27 +0800

    调整代理池数量上限

--- proxypool/setting.py
+++ proxypool/setting.py
@@ -44,7 +44,7 @@ PROXY_SCORE_MIN = 0
 PROXY_SCORE_INIT = 10
 
 # definition of proxy number
-PROXY_NUMBER_MAX = 100
+PROXY_NUMBER_MAX = 1000
 PROXY_NUMBER_MIN = 0
 
 # definition of tester cycle, it will test every CYCLE_TESTER second
